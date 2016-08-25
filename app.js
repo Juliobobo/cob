8d4887fa5d1e03504c3ad5230fec219515d28b8d
JuLiobobo <julien.bonnardel94@gmail.com>
2016-08-25 13:08:19 +0000

    port

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ var server = restify.createServer({
   name: "cob",
 });
 
-server.listen(process.env.PORT || 5000, function(){
+server.listen(5000, function(){
 	console.log('%s listening to %s', server.name, server.url); 
 });
 
